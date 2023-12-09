79febcae80e3e516caf747a7d7994efb1a649dfb
baskar <baskar15082000@gmail.com>
2023-12-09 12:17:46 +0530

    Knex

--- src/output/problem4.js
+++ src/output/problem4.js
@@ -1,1 +1,12 @@
-[{"bowler":"RN ten Doeschate","economy":"4.0000"},{"bowler":"J Yadav","economy":"4.1429"},{"bowler":"V Kohli","economy":"5.4545"},{"bowler":"R Ashwin","economy":"5.8718"},{"bowler":"S Nadeem","economy":"6.1429"},{"bowler":"Parvez Rasool","economy":"6.2000"},{"bowler":"MC Henriques","economy":"6.5600"},{"bowler":"Z Khan","economy":"6.5793"},{"bowler":"M Vijay","economy":"7.0000"},{"bowler":"GB Hogg","economy":"7.0476"}]
+[
+  { bowler: "RN ten Doeschate", economy: "4.0000" },
+  { bowler: "J Yadav", economy: "4.1429" },
+  { bowler: "V Kohli", economy: "5.4545" },
+  { bowler: "R Ashwin", economy: "5.8718" },
+  { bowler: "S Nadeem", economy: "6.1429" },
+  { bowler: "Parvez Rasool", economy: "6.2000" },
+  { bowler: "MC Henriques", economy: "6.5600" },
+  { bowler: "Z Khan", economy: "6.5793" },
+  { bowler: "M Vijay", economy: "7.0000" },
+  { bowler: "GB Hogg", economy: "7.0476" },
+];
